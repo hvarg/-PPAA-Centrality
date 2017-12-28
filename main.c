e8e4c98278c814dc13cd104f9ef6929d634fba4d
Hernán Vargas <hernan.vargas@alumnos.usm.cl>
2017-12-28 20:17:54 -0300

    simple main test

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "threadpool.h"
+#include "sgfile.h"
 
 
 /* Aqui se guardaran los segmentos a calcular y su resultado. */
@@ -19,34 +19,59 @@ void *__cent(void *DT)
 
 int main(int argc, const char * args[])
 {
-  int   R  = (argc > 1) ? atoi(args[1]) : 10,
-        NT = (argc > 2) ? atoi(args[2]) : 4,
-        i, gap;
-  struct data  **D;
-  pool  *P;
-
-  printf("Range: %d\n", R);
+  int i;
+  
+  /* Arguments */
+  if (argc < 2) {
+    printf("Modo de uso: ./centrality archivo.sg [#threads]\n");
+    return EXIT_SUCCESS;
+  }
+  char  *filename = (char*) args[1]; 
+  int   NT = (argc > 2) ? atoi(args[2]) : 4;
+  printf("File: %s\n", filename);
   printf("Threads: %d\n", NT);
-  gap = (R%NT == 0) ? R/NT : (R/NT)+1;
-  D   = (struct data**) malloc(sizeof(struct data*)*NT);
-  P   = pool_create(NT);
-
+  
+  /* Open graph */
+  graph *G = open_sg(filename);
+  if (G == NULL) 
+    return EXIT_FAILURE;
+  
+  /* Create pool */
+  int   gap = (G->size%NT == 0) ? G->size/NT : (G->size/NT)+1;
+  pool  *P  = pool_create(NT);
+  
+  /* Create data structures for each job. */
+  struct data  **D = (struct data**) malloc(sizeof(struct data*)*NT);
   for (i = 0; i < NT; i++) {
     D[i] = (struct data*) malloc(sizeof(struct data));
     D[i]->start = i*gap;
-    D[i]->end   = ((i+1)*gap - 1) < R ? ((i+1)*gap -1) : R;
+    D[i]->end   = ((i+1)*gap - 1) < G->size ? ((i+1)*gap -1) : G->size - 1;
   }
 
+  /* Send jobs to pool */
   for (i = 0; i < NT; i++) {
     pool_send_job(P, __cent, D[i]);
   }
   pool_wait(P);
   pool_del(P);
 
+  /* Free Data */
   for (i = 0; i < NT; i++) {
     free(D[i]);
   }
   free(D);
+
+  /* Print graph 
+  int j;
+  for (i = 0; i < G->size; i++) {
+    printf("%d:", i);
+    for (j = 0; j < G->nsize[i]; j++) {
+      printf(" %d", G->neigh[i][j]);
+    }
+    printf("\n");
+  }*/
+
+  graph_del(G);
 
   return EXIT_SUCCESS;
 }
